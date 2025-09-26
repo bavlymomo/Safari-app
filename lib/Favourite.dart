ff3964788511207b69e3bad467d734ba9d96d32c
bavlymomo <bavlymomtaz10@gmail.com>
2025-09-26 03:43:11 +0300

    put the navigator for pages

---
+++ lib/Favourite.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Favourite extends StatefulWidget {
+  const Favourite({super.key});
+
+  @override
+  State<Favourite> createState() => _FavouriteState();
+}
+
+class _FavouriteState extends State<Favourite> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("favourite", style: TextStyle(fontSize: 30)));
+  }
+}
